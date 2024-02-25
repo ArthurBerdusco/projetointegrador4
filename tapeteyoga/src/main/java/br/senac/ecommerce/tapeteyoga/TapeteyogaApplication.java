1927ca17e68310da25a9d974b4351bce518454a3
Arthur <arthur.berdusco1@gmail.com>
2024-02-25 17:08:05 -0300

    iniciado projeto

---
+++ tapeteyoga/src/main/java/br/senac/ecommerce/tapeteyoga/TapeteyogaApplication.java
@@ -0,0 +1,13 @@
+package br.senac.ecommerce.tapeteyoga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TapeteyogaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TapeteyogaApplication.class, args);
+	}
+
+}
---
+++ tapeteyoga/src/test/java/br/senac/ecommerce/tapeteyoga/TapeteyogaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.senac.ecommerce.tapeteyoga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TapeteyogaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
