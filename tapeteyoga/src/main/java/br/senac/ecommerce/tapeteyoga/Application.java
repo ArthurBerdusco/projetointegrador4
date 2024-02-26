7ba0b7423084fcd7edc030e7c44c5db8430d879f
Arthur <107810609+ArthurBerdusco@users.noreply.github.com>
2024-02-26 21:57:49 +0000

    concluido modelo usuario e ajustado app.properties server.port

--- tapeteyoga/src/main/java/br/senac/ecommerce/tapeteyoga/Application.java
+++ tapeteyoga/src/main/java/br/senac/ecommerce/tapeteyoga/Application.java
@@ -3,11 +3,22 @@ package br.senac.ecommerce.tapeteyoga;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
 
+        // Obtém o endereço IP da máquina
+        try {
+            InetAddress inetAddress = InetAddress.getLocalHost();
+            String ip = inetAddress.getHostAddress();
+            System.out.println("O servidor está rodando em: http://" + ip + ":8080");
+        } catch (UnknownHostException e) {
+            System.err.println("Erro ao obter o endereço IP: " + e.getMessage());
+        }
+    }
 }
