c2e6575af76fd974564c134bc961a994f67f5986
juancalebbarros <Jcaleb1936@gmail.com>
2024-03-02 19:46:26 -0300

    ListarUsuario Alterado

--- tapeteyoga/src/main/java/br/senac/ecommerce/tapeteyoga/controller/backoffice/ListarUsuarioController.java
+++ tapeteyoga/src/main/java/br/senac/ecommerce/tapeteyoga/controller/backoffice/ListarUsuarioController.java
@@ -1,15 +1,47 @@
 package br.senac.ecommerce.tapeteyoga.controller.backoffice;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import br.senac.ecommerce.tapeteyoga.model.Usuario;
+
+import java.util.List;
 
 @Controller
+@RequestMapping("/administrador/usuarios")
 public class ListarUsuarioController {
+    @Autowired
+    private Usuario usuarioService;
+
+    @ModelAttribute("filtro")
+    public Usuario getFiltro() {
+        return new Usuario(); // Crie um objeto de filtro vazio
+    }
     
     @GetMapping("/backoffice/listar-usuarios")
-    public String listarUsuario() {
+    public String listarUsuario(Model model, @RequestParam(required = false)String nome) {
+        List<Usuario> usuarios;
+        if (nome != null && !nome.isBlank()) {
+            usuarios = usuarioService.buscarPorNome(nome);
+        } else {
+            usuarios = ((Object) usuarioService).listarUsuario();
+        }
+
+        model.addAttribute("usuarios", usuarios);
         return "backoffice/listar-usuarios";
     }
     
 }
+
+
+
+
+    
+
+    
+
+
