d7b2f1a609cdc146276ef0f6b1e30b27a7159069
ArthurBerdusco <107810609+ArthurBerdusco@users.noreply.github.com>
2024-03-06 12:11:26 -0300

    finalizado logica habilitar e desabilitar com kayke

--- tapeteyoga/src/main/java/br/senac/ecommerce/tapeteyoga/controller/backoffice/CadastrarUsuarioController.java
+++ tapeteyoga/src/main/java/br/senac/ecommerce/tapeteyoga/controller/backoffice/CadastrarUsuarioController.java
@@ -1,7 +1,5 @@
 package br.senac.ecommerce.tapeteyoga.controller.backoffice;
 
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
