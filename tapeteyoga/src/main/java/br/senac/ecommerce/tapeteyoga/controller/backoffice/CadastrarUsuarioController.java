1a2bfdc2a10566d91e31a8f82e67e9d078fce1d7
Kayke <kaykesprfc@gmail.com>
2024-03-06 12:43:10 -0300

    Concluido logica de autentificacao de usuarios habilitados e desabilitados

--- tapeteyoga/src/main/java/br/senac/ecommerce/tapeteyoga/controller/backoffice/CadastrarUsuarioController.java
+++ tapeteyoga/src/main/java/br/senac/ecommerce/tapeteyoga/controller/backoffice/CadastrarUsuarioController.java
@@ -3,6 +3,7 @@ package br.senac.ecommerce.tapeteyoga.controller.backoffice;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
@@ -41,7 +42,7 @@ public class CadastrarUsuarioController {
         return modelAndView;
     }
 
-    @PostMapping("/setup")
+    @GetMapping("/setup")
     public ResponseEntity<?> cadastrarUsuario() {
 
         Usuario usuario = new Usuario();
