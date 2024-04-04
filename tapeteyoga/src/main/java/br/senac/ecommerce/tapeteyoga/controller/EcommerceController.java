f453f60abc6855e42238adc9bcd6e689d3df3653
Arthur <arthur.berdusco1@gmail.com>
2024-04-04 00:31:48 -0300

    atualizado header da loja

--- tapeteyoga/src/main/java/br/senac/ecommerce/tapeteyoga/controller/EcommerceController.java
+++ tapeteyoga/src/main/java/br/senac/ecommerce/tapeteyoga/controller/EcommerceController.java
@@ -2,11 +2,20 @@ package br.senac.ecommerce.tapeteyoga.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import br.senac.ecommerce.tapeteyoga.repository.UsuarioRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+
 
 @Controller
 public class EcommerceController {
 
   @Autowired
   UsuarioRepository repository;
+
+  @GetMapping("/")
+  public String index() {
+    System.out.println("KKKKKKKKKKKKKKKKKKKKKK");
+      return "index";
+  }
+  
 
 }
